f01634eb43840b76d075de2f2616a1ea3c7df81e
edykhne <edykhne@uoguelph.ca>
2020-10-24 00:11:46 -0400

    put finaly done. fixed iteration of destinations. added who has file command

--- mpish.c
+++ mpish.c
@@ -5,11 +5,13 @@
 #include <string.h>
 #include <libgen.h>
 #include <unistd.h>
+#include <sys/stat.h> 
 #include <sys/types.h> 
 #include <sys/wait.h>
 
 const int MAX_INPUT_CHARS = 4096;
-enum Commands {put, get, ls, lls, lrm, end, unknown};
+enum Commands {put, get, ls, lls, lrm, checkHave, end, unknown};
+int lastSent = 1;
 
 void getInput(int * commSz, enum Commands * command, char * fileName[], int * procNum, char *** args) {
     char * endPtr = NULL;
@@ -36,7 +38,6 @@ void getInput(int * commSz, enum Commands * command, char * fileName[], int * pr
             return;
         }
         temp = strtok(NULL, " ");
-        printf("tempcheck: %s\n", temp);
         if(temp != NULL && strcmp(temp, "") != 0) {
             *procNum = strtol(temp, &endPtr, 10);
             if(temp == endPtr || *procNum <= 0 || *procNum > (*commSz - 1)){
@@ -47,7 +48,6 @@ void getInput(int * commSz, enum Commands * command, char * fileName[], int * pr
         
         *command = put;
     } else if (strcmp(temp, "get") == 0) {
-        fflush(stdout);
         *fileName = strtok(NULL, " ");
         if(*fileName == NULL || strcmp(*fileName, "") == 0) {
             return;
@@ -60,14 +60,19 @@ void getInput(int * commSz, enum Commands * command, char * fileName[], int * pr
     } else if (strcmp(temp, "lrm") == 0) {
         char ** res  = NULL;
         int numSpaces = 0;
+
+        res = realloc(res, sizeof (char*) * ++numSpaces);
+        res[numSpaces-1] = "rm";
+        temp = strtok(NULL, " ");
+
         while (temp) {
-            res = realloc (res, sizeof (char*) * ++numSpaces);
+            res = realloc(res, sizeof (char*) * ++numSpaces);
             res[numSpaces-1] = temp;
-            temp = strtok (NULL, " ");
+            temp = strtok(NULL, " ");
         }
 
         /* realloc one extra element for the last NULL */
-        res = realloc (res, sizeof (char*) * (numSpaces+1));
+        res = realloc(res, sizeof (char*) * (numSpaces+1));
         res[numSpaces] = NULL;
         *args = res;
         *command = lrm;
@@ -86,6 +91,65 @@ void printUsage() {
     printf("exit\n");
 }
 
+void storageProcPut(int myRank){
+    int length;
+    MPI_Status status;
+
+    //fileName
+    MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+    MPI_Get_count(&status, MPI_CHAR, &length);
+    char * fileName = (char*)malloc(sizeof(char) * length);
+    MPI_Recv(fileName, length, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    //fileContents
+    MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+    MPI_Get_count(&status, MPI_BYTE, &length);
+    unsigned char * content = (unsigned char*)malloc(sizeof(unsigned char) * length);
+    MPI_Recv(content, length, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    fflush(stdout);
+    char filePath[MAX_INPUT_CHARS];
+    char strNum[99]; 
+    sprintf(strNum, "%d", myRank); 
+    strcpy(filePath, "p");
+    strcat(filePath, strNum);
+    strcat(filePath, "/");
+    strcat(filePath, fileName);
+
+    MPI_File file;
+    MPI_File_open(MPI_COMM_SELF, filePath, MPI_MODE_RDWR | MPI_MODE_CREATE, MPI_INFO_NULL, &file);
+    MPI_File_write(file, content, length, MPI_BYTE, MPI_STATUS_IGNORE);
+    MPI_File_close(&file);
+    free(content);
+    free(fileName);
+}
+
+void sendCheckHaveFile(int myRank) {
+    int length;
+    MPI_Status status;
+    MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+    MPI_Get_count(&status, MPI_CHAR, &length);
+    char * fileName = (char*)malloc(sizeof(char) * length);
+    MPI_Recv(fileName, length, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    fflush(stdout);
+    char filePath[MAX_INPUT_CHARS];
+    char strNum[99]; 
+    sprintf(strNum, "%d", myRank); 
+    strcpy(filePath, "p");
+    strcat(filePath, strNum);
+    strcat(filePath, "/");
+    strcat(filePath, fileName);
+
+    int response;
+    if(access(filePath, F_OK ) == -1 ) {
+        response = -1;
+    }else{
+        response = myRank;
+    }
+    MPI_Send(&response, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+}
+
 void storageProcess(int *myRank, int *commSz, bool *endProg) {
     enum Commands command;
     MPI_Status status;
@@ -93,10 +157,9 @@ void storageProcess(int *myRank, int *commSz, bool *endProg) {
     // MPI_Get_count(&status, MPI_CHAR, &number_amount);
     // status.MPI_TAG
     MPI_Recv(&command, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    printf("%d\n", command);
     switch (command){
         case put:
-            // statements
+            storageProcPut(*myRank);
             break;
 
         case get:
@@ -106,13 +169,8 @@ void storageProcess(int *myRank, int *commSz, bool *endProg) {
         case ls:
             // statements
             break;
-
-        case lls:
-            // statements
-            break;
-
-        case lrm:
-            // statements
+        case checkHave:
+            sendCheckHaveFile(*myRank);
             break;
 
         case end:
@@ -125,8 +183,57 @@ void storageProcess(int *myRank, int *commSz, bool *endProg) {
     }
 }
 
-void shellPut(int *commSz){
+int whoHasFile(int commSz, char * baseFileName){
+    enum Commands command = checkHave;
+    int have = 0;
+    int loc = -1;
+    for (int i = 1; i < commSz; i++) {
+        //telling what the process needs to do
+        MPI_Send(&command, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        MPI_Send(baseFileName, strlen(baseFileName) + 1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+    }
+    for (int i = 1; i < commSz; i++) {
+        MPI_Recv(&have, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(have == 1) {
+            loc = i;
+        }
+    }
+    return loc;
+}
 
+void shellPut(int *commSz, int command, char * fileName, char * baseFileName, int procNum){
+    if( access(fileName, F_OK ) == -1 ) {
+        printf("File does not exist");
+        return;
+    }
+    
+    if(procNum == -1){
+        procNum = whoHasFile(*commSz, baseFileName);
+        if(procNum == -1){
+            procNum = lastSent;
+            lastSent = (lastSent) % (*commSz - 1) + 1;
+        }
+    }
+
+    //telling what the process needs to do
+    MPI_Send(&command, 1, MPI_INT, procNum, 0, MPI_COMM_WORLD);
+    
+    //sendTitle
+    MPI_Send(baseFileName, strlen(baseFileName) + 1, MPI_CHAR, procNum, 0, MPI_COMM_WORLD);
+
+    //sendFile
+    MPI_File  file;
+    MPI_Status status;
+    struct stat st;
+
+    stat(fileName, &st);
+    int size = st.st_size;
+    unsigned char * contents[size];
+    fflush(stdout);
+    MPI_File_open(MPI_COMM_SELF, fileName, MPI_MODE_RDWR, MPI_INFO_NULL, &file);
+    MPI_File_read(file, contents, size, MPI_BYTE, MPI_STATUS_IGNORE);
+    MPI_Send(contents, size, MPI_BYTE, procNum, 0, MPI_COMM_WORLD);
+    MPI_File_close(&file);
 }
 
 void shellGet(enum Commands command, int *commSz){
@@ -182,7 +289,7 @@ void shellProcess(int *commSz, bool *endProg) {
     printf("%d | %s | %s | %d\n", command, fileName, baseFileName, procNum);
     switch (command){
         case put:
-            // statements
+            shellPut(commSz, command, fileName, baseFileName, procNum);
             break;
 
         case get:
@@ -199,7 +306,7 @@ void shellProcess(int *commSz, bool *endProg) {
 
         case lrm:
             runRm(&argv);
-            free(*argv);
+            free(argv);
             break;
 
         case end:
@@ -229,6 +336,13 @@ int main(void) {
 
     if(myRank == 0) {
         printf("\n");
+    } else {
+        char str[100];
+        char strNum[99]; 
+        sprintf(strNum, "%d", myRank); 
+        strcpy(str, "p");
+        strcat(str, strNum);
+        mkdir(str, 0777);
     }
         
     bool endProg = false;
